b47ae7e30203c50dcd6691c07380b4e808255b75
am1t0 <amiitt43@gmail.com>
2025-08-23 21:34:40 +0530

    overlay opacity removed and rgb used

--- client/src/sections/projects/Forms/DetailEdit.jsx
+++ client/src/sections/projects/Forms/DetailEdit.jsx
@@ -3,15 +3,24 @@ import './Forms.css'
 
 export default function DetailEdit(props) {
   
+  const { setEditMode } = props;
+
   return (
     <form className='project-edit-form'>
+       <h4>Project Details</h4>
        <div className="project-form-item">
          <label htmlFor="description">Description</label>
          <textarea  id="description"></textarea>
        </div>
-       <div className="project-form-item">
+       <div id="se-dates-container">
+         <div className="start-end-date">
+          <label htmlFor="start">start</label>
           <input type="date" placeholder='start'/>
+         </div>
+         <div className="start-end-date">
+           <label htmlFor="end">end</label>
           <input type='date' placeholder='end'/>
+         </div>
        </div>
        <div className="project-form-item">
           <label htmlFor="budget">Budget</label>
@@ -20,6 +29,12 @@ export default function DetailEdit(props) {
        <div className="project-form-item">
           <label htmlFor="contractor">Contractor</label>
            <input type="text" />
+       </div>
+
+       <div className="two-options">
+         <button type='button' className='submit-btn' onClick={()=> {alert("loda lega!!")}}> submit </button>
+
+         <button type='button' className='cancel-btn' onClick={()=> setEditMode((e)=> !e)}> cancel </button>
        </div>
     </form>
   )
